2cdd15b8f85ea0ae36470dc9cd58217bd5028aba
nusera <nusera.neha@gmail.com>
2022-03-25 00:00:21 -0400

    created player constructors+getset

--- src/main/java/SlotsPlayer.java
+++ src/main/java/SlotsPlayer.java
@@ -1,2 +1,36 @@
 public class SlotsPlayer {
+    int age;
+    String name;
+    double funds;
+
+    public SlotsPlayer(String name, int age, double funds) {
+        this.name = name;
+        this.age = age;
+        this.funds = funds;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getFunds() {
+        return funds;
+    }
+
+    public void setFunds(double funds) {
+        this.funds = funds;
+    }
+
 }
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -3,4 +3,6 @@ import org.junit.Test;
 
 public class PlayerTest {
 
+
+
 }
